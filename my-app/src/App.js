f37293cb1b25b38a4a1cf8d7bdf8f95d60b66b25
SergiuRusu12 <sergiurusu122@gmail.com>
2024-01-07 02:49:44 +0200

    signup si login cu baza de date

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
-import LoginSignup from "./components/LoginSignup";
+import LoginSignup from "./components/LoginSignup.js";
+import ProjectsPage from "./components/ProjectsPage.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <LoginSignup />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LoginSignup />} />
+        <Route exact path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- my-app/src/components/LoginSignup.js
+++ my-app/src/components/LoginSignup.js
@@ -4,9 +4,114 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const LoginSignup = () => {
   const [action, setAction] = useState("Sign Up");
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    const email = document.querySelector('input[type="email"]')?.value;
+    const username = document.querySelector('input[type="text"]')?.value;
+    const password = document.querySelector('input[type="password"]')?.value;
+    let userType;
+    if (action !== "Login") {
+      userType = document.querySelector('input[name="role"]:checked')?.value;
+    }
+
+    if (action === "Login") {
+      try {
+        const usersResponse = await fetch("http://localhost:9000/api/users");
+        if (!usersResponse.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const users = await usersResponse.json();
+        const user = users.find((u) => u.UserName === username);
+
+        if (!user) {
+          alert("User not found");
+          return;
+        }
+        const userResponse = await fetch(
+          `http://localhost:9000/api/user/${user.UserID}`
+        );
+        if (!userResponse.ok) {
+          throw new Error("Failed to fetch user details");
+        }
+        const userData = await userResponse.json();
+
+        if (!userData || userData.Password !== password) {
+          alert("Invalid username/password");
+          return;
+        }
+
+        navigate("/projects");
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    } else {
+      try {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+          alert("Please enter a valid email address");
+          return;
+        }
+        const userTypeRadio = document.querySelector(
+          'input[name="role"]:checked'
+        );
+        if (!userTypeRadio) {
+          alert("Please select a user type");
+          return;
+        }
+        userType = userTypeRadio.value;
+
+        const usersResponse = await fetch("http://localhost:9000/api/users");
+        if (!usersResponse.ok) {
+          throw new Error("Failed to fetch users");
+        }
+        const users = await usersResponse.json();
+        const userExists = users.some(
+          (user) => user.UserName === username || user.Email === email
+        );
+
+        if (userExists) {
+          alert("Username or Email already in use");
+          return;
+        }
+
+        const newUser = {
+          UserName: username,
+          Email: email,
+          Password: password,
+          UserType: userType,
+        };
+
+        const response = await fetch("http://localhost:9000/api/user", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newUser),
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to create user");
+        }
+
+        const responseData = await response.json();
+        console.log("Response Data:", responseData);
+
+        if (responseData) {
+          console.log("User created successfully, navigating to /projects");
+          navigate("/projects");
+        } else {
+          console.error("Failed to create user:", responseData.msg);
+        }
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    }
+  };
 
   return (
     <div className="container">
@@ -69,7 +174,7 @@ const LoginSignup = () => {
           Create Account
         </div>
       </div>
-      <button className="submit" id="subBtn">
+      <button className="submit" id="subBtn" onClick={handleSubmit}>
         {action === "Login" ? "Login" : "Sign Up"}
       </button>
     </div>
